26dba12fb5d39b4b2078725a6cea6982eac254e5
HoangDinhLyHuy <hoangdinhlyhuy@gmail.com>
2024-06-24 23:11:30 +0700

    update dark mode

---
+++ index.js
@@ -0,0 +1,22 @@
+// START TESTIMONIALS
+// owlcarousel config
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+  $('.owl-carousel').owlCarousel({
+    margin: 20,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        768:{
+            items:1,
+        },
+        992:{
+            items:2,
+        }
+    }
+})
+// END TESTIMONIALS
